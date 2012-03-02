9dd1af5bf22c2effcf8ba1c4ddaff711831e67eb
cexbrayat <cexbrayat@gmail.com>
2012-03-02 01:09:08 +0100

    activities are filtered

--- notifyit.js
+++ notifyit.js
@@ -1,37 +1,52 @@
 var io  = require('socket.io');
-var http = require("http");
+var http = require('http');
 
 var server = http.createServer(function(request, response) {
+  //sur la reception d'une requete http
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Notifyit is used to notify an activity to logged users in real time");
+
+  //on recupere les données en post
   var fullBody = '';
   request.on("data", function(postDataChunk) {
-      console.log("Received POST data chunk '" + postDataChunk + "'.");
-      fullBody += postDataChunk;
+    fullBody += postDataChunk;
   });
+
+  //quand toutes les données sont recuperees
   request.on('end', function() {
-      var postData = JSON.parse(fullBody);
-      console.log("users interested '" + postData.users + "'.");
-      console.log('connecteds ' + connecteds);
-      var socket = connecteds["cedric"];
-      console.log("socket : " + socket);
+    var postData = JSON.parse(fullBody);
+
+    //on recupere les users interesses par la notif
+    console.log("users interested '" + postData.users + "'.");
+
+    for(user in postData.users){
+      //on recupere la socket associee
+      var socket = connecteds[postData.users[user]];
+      //si le user est connecte, alors on envoie le message
       if(socket != undefined){
+        console.log('activity sent to ' + postData.users[user]);
       	socket.emit('activity', { data: postData.content });
       }
-    });
+	  }
+  });
+
   response.end();
 });
 
+//liste des users connectes
 var connecteds = {};
 
-var socket = io.listen(server);
-socket.sockets.on('connection', function (socket) {
+//websocket
+var websocket = io.listen(server);
+//a la connexion
+websocket.sockets.on('connection', function (socket) {
   console.log('client');
+
   socket.on('user', function (user) {
+  	//on stocke le login du user
     console.log('user ' + user.name);
-	delete connecteds[user.name];
+    delete connecteds[user.name];
     connecteds[user.name] = socket;
-    console.log('connecteds ' + connecteds);
   });
 });
 
